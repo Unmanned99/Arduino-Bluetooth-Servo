2546daf12bfa0962431dd5ed935e3d048af120d5
Unmanned99 <vrl.tumer@gmail.com>
2021-05-22 02:16:31 +0300

    Create Bluetooth_Servo_motor2.ino
    Bluetooth Servo

---
+++ Bluetooth_Servo_motor2.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo motor;
+int pozisyon=0;
+int pos=0;
+
+void setup() {
+  motor.attach(9);
+
+
+}
+
+void loop() {
+  
+    for (pos = 0;pos<=180;pos+=1)
+    {
+      motor.write(pos);
+      delay(15);
+    }
+    for (pos=180;pos>=180;pos-=1)
+    {
+      motor.write(pos);
+      delay(15);
+    }
+
+}
